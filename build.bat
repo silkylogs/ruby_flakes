faa1c9dedb9a179df6ffffc811b933a648beefb9
silkylogs <silkylogs@gmail.com>
2023-12-06 20:20:19 +0600

    Add -g flag to build script

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-set flags=-Wall -Werror -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wsign-compare -Wlogical-op -Wtype-limits -Wsuggest-attribute=pure -Wsuggest-attribute=const -Wsuggest-attribute=noreturn -Wsuggest-attribute=format -Wformat-nonliteral -ansi -pedantic -O0 -Wextra
+set flags=-Wall -Werror -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wsign-compare -Wlogical-op -Wtype-limits -Wsuggest-attribute=pure -Wsuggest-attribute=const -Wsuggest-attribute=noreturn -Wsuggest-attribute=format -Wformat-nonliteral -ansi -pedantic -O0 -Wextra -g
 
 gcc.exe %flags% ruby_flakes.c -o ruby_flakes.exe
