0e81a8ec8f155f1f7bc0957088410b2d66046ff4
silkylogs <silkylogs@gmail.com>
2023-12-07 23:03:33 +0600

    rf_Array: Removed redundant braces from CHECKED_INIT

--- rf_Array.c
+++ rf_Array.c
@@ -49,15 +49,13 @@ void rf_Array_unchecked_init(
 }
 
 #define RF_ARRAY_CHECKED_INIT(RF_ARR_PTR, NEW_BYTE_ARR, NEW_BYTE_ARR_LEN, NEW_ELEM_LEN, NEW_ELEM_CNT) \
-	do {								\
-		RF_ASSERT((RF_ARR_PTR) != NULL);			\
-		RF_ASSERT((NEW_BYTE_ARR) != NULL);			\
-		RF_ASSERT((NEW_BYTE_ARR_LEN) > 0);			\
-		RF_ASSERT((NEW_ELEM_LEN) > 0);				\
-		RF_ASSERT((NEW_ELEM_CNT) > 0);				\
-		RF_ASSERT((NEW_BYTE_ARR_LEN) <= (NEW_ELEM_LEN) * (NEW_ELEM_CNT)); \
-		rf_Array_unchecked_init((RF_ARR_PTR), (NEW_BYTE_ARR), (NEW_ELEM_LEN), (NEW_ELEM_CNT)); \
-	} while(0)
+	RF_ASSERT((RF_ARR_PTR) != NULL);				\
+	RF_ASSERT((NEW_BYTE_ARR) != NULL);				\
+	RF_ASSERT((NEW_BYTE_ARR_LEN) > 0);				\
+	RF_ASSERT((NEW_ELEM_LEN) > 0);					\
+	RF_ASSERT((NEW_ELEM_CNT) > 0);					\
+	RF_ASSERT((NEW_BYTE_ARR_LEN) <= (NEW_ELEM_LEN) * (NEW_ELEM_CNT)); \
+	rf_Array_unchecked_init((RF_ARR_PTR), (NEW_BYTE_ARR), (NEW_ELEM_LEN), (NEW_ELEM_CNT)); \
 
 #define RF_ARRAY_UNCHECKED_INDEX_ACCESS(RF_ARR, TYPE, IDX) 	\
 	  RF_CAST((RF_ARR).mem_as_bytes, TYPE*)[(IDX)]
