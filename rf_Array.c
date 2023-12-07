bafd4599cd8b31bfcfec73cf85dda840092722aa
silkylogs <silkylogs@gmail.com>
2023-12-07 21:34:53 +0600

    rf_Memory: Remove stray header and group related functionality

--- rf_Array.c
+++ rf_Array.c
@@ -7,7 +7,10 @@ struct rf_Array {
 	isize elem_cnt;
 };
 
-/* TODO: maybe use ranged pointers to express subarrays? */
+/*
+ * TODO: maybe use ranged pointers to express subarrays?
+ * Note: do this only when theres a demonstratable need for it
+ */
 struct rf_Memory_fat_ptr;
 
 b32 rf_Array_check_assumptions(void);
@@ -16,11 +19,7 @@ void rf_Array_unchecked_init(
 	u8 *new_byte_array,
 	isize new_elem_length,
 	isize new_elem_count);
-void rf_Array_unchecked_index_write(
-	struct rf_Array *rf_arr,
-	u8 *const elem_bytes,
-	isize elem_length,
-	isize index);
+
 
 b32 rf_Array_check_assumptions() {
 	b32 all_ok;
@@ -49,17 +48,6 @@ void rf_Array_unchecked_init(
 	rf_arr->elem_cnt = new_elem_count;
 }
 
-#define RF_ARRAY_UNCHECKED_INDEX_ACCESS(RF_ARR, TYPE_PTR, IDX) 	\
-	  RF_CAST((RF_ARR).mem_as_bytes, TYPE_PTR)[(IDX)]
-
-#define RF_ARRAY_CHECKED_INDEX_ACCESS(RF_ARR, TYPE_PTR, IDX)		\
-	/* Index bound checks */					\
-	RF_ASSERT((IDX) >= 0);						\
-	RF_ASSERT((IDX) < (RF_ARR).elem_cnt);				\
-									\
-	RF_ARRAY_UNCHECKED_INDEX_ACCESS(RF_ARR, TYPE_PTR, IDX)
-	
-
 #define RF_ARRAY_CHECKED_INIT(RF_ARR_PTR, NEW_BYTE_ARR, NEW_BYTE_ARR_LEN, NEW_ELEM_LEN, NEW_ELEM_CNT) \
 	do {								\
 		RF_ASSERT((RF_ARR_PTR) != NULL);			\
@@ -70,6 +58,16 @@ void rf_Array_unchecked_init(
 		RF_ASSERT((NEW_BYTE_ARR_LEN) <= (NEW_ELEM_LEN) * (NEW_ELEM_CNT)); \
 		rf_Array_unchecked_init((RF_ARR_PTR), (NEW_BYTE_ARR), (NEW_ELEM_LEN), (NEW_ELEM_CNT)); \
 	} while(0)
+
+#define RF_ARRAY_UNCHECKED_INDEX_ACCESS(RF_ARR, TYPE_PTR, IDX) 	\
+	  RF_CAST((RF_ARR).mem_as_bytes, TYPE_PTR)[(IDX)]
+
+#define RF_ARRAY_CHECKED_INDEX_ACCESS(RF_ARR, TYPE_PTR, IDX)		\
+	/* Index bound checks */					\
+	RF_ASSERT((IDX) >= 0);						\
+	RF_ASSERT((IDX) < (RF_ARR).elem_cnt);				\
+									\
+	RF_ARRAY_UNCHECKED_INDEX_ACCESS(RF_ARR, TYPE_PTR, IDX)
 
 
 #endif
