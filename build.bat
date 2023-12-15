ce0ad45952bb719bb04108ac8d90bbe14f45d5be
silkylogs <silkylogs@gmail.com>
2023-12-15 13:29:47 +0600

    Pretty format build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,19 @@
-set flags=-Wall -Werror -Wwrite-strings -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wsign-compare -Wlogical-op -Wtype-limits -Wsuggest-attribute=pure -Wsuggest-attribute=const -Wsuggest-attribute=noreturn -Wsuggest-attribute=format -Wformat-nonliteral -ansi -pedantic -O0 -Wextra -g
+set warning_flags=	^
+-Wall			^
+-Wextra			^
+-Wwrite-strings		^
+-Wstrict-prototypes	^
+-Wmissing-prototypes	^
+-Wmissing-declarations	^
+-Wsign-compare		^
+-Wlogical-op		^
+-Wtype-limits		^
+-Wsuggest-attribute=pure	^
+-Wsuggest-attribute=const	^
+-Wsuggest-attribute=noreturn	^
+-Wsuggest-attribute=format	^
+-Wformat-nonliteral
 
-gcc.exe %flags% ruby_flakes.c -o ruby_flakes.exe
+set flags=-ansi -pedantic -O0 -g -Werror 
+
+gcc.exe %warning_flags% %flags% ruby_flakes.c -o ruby_flakes.exe
